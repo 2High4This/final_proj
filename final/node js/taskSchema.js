0effd69ac6221686f2236fd7a66b5917cb12847f
2High4This <batatgoyava@gmail.com>
2022-12-08 22:02:42 +0200

    made task schema beautiful

--- final/node js/taskSchema.js
+++ final/node js/taskSchema.js
@@ -1,26 +1,25 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const tasksSchema = new Schema({
-    Usernames: { type: Array },
+  Usernames: { type: Array },
 
-    TaskName: {
-        type: String,
-        minLength: 1,
-        maxLength: 50,
-        required: true
-    },
+  TaskName: {
+    type: String,
+    minLength: 1,
+    maxLength: 50,
+    required: true,
+  },
 
-    startDate: {
-        type: Date,
-        required: true
-    },
+  startDate: {
+    type: Date,
+    required: true,
+  },
 
-    endDate: {
-        type: Date,
-        required: true
-    }
+  endDate: {
+    type: Date,
+    required: true,
+  },
 });
 
-module.exports = mongoose.model('tasks', tasksSchema);
-
+module.exports = mongoose.model("tasks", tasksSchema);
